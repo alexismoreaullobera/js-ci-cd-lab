448b7f5120ffd577775a7049e72c2a4088b91c0a
Alexis Moreau <alexis.moreau.llobera@gmail.com>
2025-12-03 18:26:18 +0100

    cd: enable github pages deployment

--- src/sum.js
+++ src/sum.js
@@ -2,4 +2,12 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = { sum };
+// Export pour Node (Jest)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { sum };
+}
+
+// Export pour le navigateur (window.sum)
+if (typeof window !== 'undefined') {
+    window.sum = sum;
+}
